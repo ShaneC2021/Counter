4458366e249c347e1d2fc7c8ee3ade4e7beee759
ShaneC2021 <84458099+ShaneC2021@users.noreply.github.com>
2021-10-04 09:17:45 -0230

    counter 1st draft

---
+++ index.js
@@ -0,0 +1,13 @@
+let num = 0;
+
+function increment() {
+  num = num + 1;
+  document.getElementById("display").value = num;
+}
+
+function decrement() {
+  num = num - 1;
+  document.getElementById("display").value = num;
+  }
+
+  
